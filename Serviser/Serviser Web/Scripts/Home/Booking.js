ad4c564ab25ffda464f6f3f331c10d3e48536770
Hussain Taj <hussaintajmonster@gmail.com>
2019-04-13 20:24:49 +0500

    Booking Map Error Fixed. Rather Identified.

--- Serviser/Serviser Web/Scripts/Home/Booking.js
+++ Serviser/Serviser Web/Scripts/Home/Booking.js
@@ -3,13 +3,13 @@
 bookingApp.controller("OptionsSelectionController", function ($scope) {
     var me = this;
     me.maxSteps = 4;
-
     $scope.currentStep = 1;
+
     $scope.optionsData = [];
     $scope.selectedVehicleTypeIndex = null;
     $scope.selectedProblemsIndices = [];
     $scope.descriptionOfProblem = '';
-    $scope.currentPos = {lat: 0, lng: 0};
+    $scope.currentPos = { lat: 0, lng: 0 };
     $scope.map = null;
     $scope.mapMarker = null;
     $scope.currentAddress = 'Map API quota is out probably';
@@ -26,18 +26,22 @@ bookingApp.controller("OptionsSelectionController", function ($scope) {
         $scope.selectedProblemsIndices[index] = !$scope.selectedProblemsIndices[index];
     }
 
+
     $scope.pinCurrentLocation = function () {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
-                if (position) {
-                    $scope.currentPos.lat = position.coords.latitude;
-                    $scope.currentPos.lng = position.coords.longitude;
-                }
+                $scope.currentPos.lat = position.coords.latitude;
+                $scope.currentPos.lng = position.coords.longitude;
                 $scope.mapMarker.setPosition($scope.currentPos);
                 $scope.map.panTo($scope.currentPos);
-            });
+            },
+                function (posErr) {
+                    alert(posErr.message);
+                });
         }
-
+        else {
+            alert("Your Browser Doesn't Support GeoLocation");
+        }
     }
 
 
@@ -50,48 +54,51 @@ bookingApp.controller("OptionsSelectionController", function ($scope) {
             $scope.currentStep--;
     }
 
-    me.getCurrentPosition = function()
-    {
+    me.getCurrentPosition = function () {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
-                if (position) {
                     $scope.currentPos.lat = position.coords.latitude;
                     $scope.currentPos.lng = position.coords.longitude;
-                }
             });
         }
     }
 
+    me.initMapWithLocation = function (position) {
+        $scope.currentPos.lat = position.coords.latitude;
+        $scope.currentPos.lng = position.coords.longitude;
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: $scope.currentPos
+        });
+
+        $scope.mapMarker = new google.maps.Marker({
+            position: $scope.currentPos,
+            map: $scope.map,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+
+        google.maps.event.addListener($scope.mapMarker, 'mouseup', function (event) {
+            $scope.currentPos = {
+                lat: $scope.mapMarker.getPosition().lat(),
+                lng: $scope.mapMarker.getPosition().lng()
+            };
+
+            $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${$scope.currentPos.lat},${$scope.currentPos.lng}&key=AIzaSyDg-T5ErBSP7PrxppZiO9OPBA3HW4TXleo`, function (data, status) {
+                if (status === 'success') {
+                    $scope.currentAddress = data.results[0].formatted_address;
+                }
+            });
+        });
+    }
+
     me.initMap = function () {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                if (position) {
-                    $scope.currentPos.lat = position.coords.latitude;
-                    $scope.currentPos.lng = position.coords.longitude;
-                }
-                $scope.map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
-                    center: $scope.currentPos
-                });
-                $scope.mapMarker = new google.maps.Marker({
-                    position: $scope.currentPos,
-                    map: $scope.map,
-                    draggable: true,
-                    animation: google.maps.Animation.DROP
-                });
-
-                google.maps.event.addListener($scope.mapMarker, 'mouseup', function (event) {
-                    $scope.currentPos = {
-                        lat: $scope.mapMarker.getPosition().lat(),
-                        lng: $scope.mapMarker.getPosition().lng()
-                    };
-                    $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${$scope.currentPos.lat},${$scope.currentPos.lng}&key=AIzaSyDg-T5ErBSP7PrxppZiO9OPBA3HW4TXleo`, function (data, status) {
-                        if (status === 'success') {
-                            $scope.currentAddress = data.results[0].formatted_address;
-                        }
-                    });
-                });
-            });
+            navigator.geolocation.getCurrentPosition(me.initMapWithLocation, me.initMapWithLocation({ coords: {latitude: 0, longitude: 0}}));
+        }
+        else {
+            me.initMapWithLocation({ coords: { latitude: 0, longitude: 0 } });
         }
     }
 
@@ -99,7 +106,6 @@ bookingApp.controller("OptionsSelectionController", function ($scope) {
 
     dummyDataInstantiate($scope);
 });
-
 
 function dummyDataInstantiate(scope) {
     scope.optionsData = [
@@ -119,7 +125,7 @@ function dummyDataInstantiate(scope) {
             Id: 4, Name: 'Van', ImageUrl: '/Assets/Images/van.png', Problems:
             [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
         },
-         {
+        {
             Id: 1, Name: 'Bike', ImageUrl: '/Assets/Images/bike.png', Problems:
             [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
         },
