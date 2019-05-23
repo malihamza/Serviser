47212d188af4c068885326d06b3e26a1c7c49771
Hussain Taj <hussaintajmonster@gmail.com>
2019-05-23 21:15:47 +0500

    A function created in DbContext that assists in API

--- Serviser/Serviser.DAL/Entity/User.cs
+++ Serviser/Serviser.DAL/Entity/User.cs
@@ -25,6 +25,15 @@ namespace Serviser.DAL.Entity
             // Add custom user claims here
             return userIdentity;
         }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
         [Required]
         [StringLength(50)]
         public string FirstName { get; set; }
