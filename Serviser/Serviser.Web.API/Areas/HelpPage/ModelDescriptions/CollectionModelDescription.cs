adf4eaf01cedfd583bb64977bca69acec3fc1d87
Hussain Taj <hussaintajmonster@gmail.com>
2019-05-23 21:16:58 +0500

    An API created. Trivial registration and login should now work.
    Source https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/individual-accounts-in-web-api

---
+++ Serviser/Serviser.Web.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Serviser.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Serviser/Serviser.Web.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Serviser.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
