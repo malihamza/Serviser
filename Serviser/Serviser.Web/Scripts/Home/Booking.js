e79fa24c11d5e88b100e34dd52f61f96aa8a1b7f
ehsaan <ehsaan90@outlook.com>
2019-05-26 04:09:53 +0500

    a

--- Serviser/Serviser.Web/Scripts/Home/Booking.js
+++ Serviser/Serviser.Web/Scripts/Home/Booking.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿//var bookingApp = angular.module("MechanicBookingApp", ['ngAnimate']);
 
 //bookingApp.controller("OptionsSelectionController", function ($scope) {
@@ -259,6 +260,63 @@ bookingApp.controller("OptionsSelectionController", function ($scope) {
             $scope.currentStep--;
     }
 >>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
+=======
+﻿var bookingApp = angular.module("MechanicBookingApp", ['ngAnimate']);
+
+bookingApp.controller("OptionsSelectionController", function ($scope) {
+    var me = this;
+    me.maxSteps = 4;
+    $scope.currentStep = 1;
+
+    $scope.optionsData = [];
+    $scope.selectedVehicleTypeIndex = null;
+    $scope.selectedProblemsIndices = [];
+    $scope.descriptionOfProblem = '';
+    $scope.currentPos = { lat: 0, lng: 0 };
+    $scope.map = null;
+    $scope.mapMarker = null;
+    $scope.currentAddress = 'Map API quota is out probably';
+
+    me.pos = new google.maps.LatLng();
+
+    $scope.step1Click = function (index) {
+        $scope.selectedVehicleTypeIndex = index;
+        $scope.selectedProblemsIndices = new Array($scope.optionsData[index].Problems.length);
+        $scope.nextStep();
+    }
+
+    $scope.step2ItemClick = function (index) {
+        $scope.selectedProblemsIndices[index] = !$scope.selectedProblemsIndices[index];
+    }
+
+
+    $scope.pinCurrentLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $scope.currentPos.lat = position.coords.latitude;
+                $scope.currentPos.lng = position.coords.longitude;
+                $scope.mapMarker.setPosition($scope.currentPos);
+                $scope.map.panTo($scope.currentPos);
+            },
+                function (posErr) {
+                    alert(posErr.message);
+                });
+        }
+        else {
+            alert("Your Browser Doesn't Support GeoLocation");
+        }
+    }
+
+
+    $scope.nextStep = function () {
+        if ($scope.currentStep < me.maxSteps)
+            $scope.currentStep++;
+    }
+    $scope.previousStep = function () {
+        if ($scope.currentStep > 1)
+            $scope.currentStep--;
+    }
+>>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
 
     me.getCurrentPosition = function () {
         if (navigator.geolocation) {
@@ -278,6 +336,7 @@ bookingApp.controller("OptionsSelectionController", function ($scope) {
             center: $scope.currentPos
         });
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 function next_view() {
     document.getElementById("problem").style.visibility = "hidden";
@@ -321,6 +380,21 @@ function next_view() {
                 lng: $scope.mapMarker.getPosition().lng()
             };
 
+=======
+        $scope.mapMarker = new google.maps.Marker({
+            position: $scope.currentPos,
+            map: $scope.map,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+
+        google.maps.event.addListener($scope.mapMarker, 'mouseup', function (event) {
+            $scope.currentPos = {
+                lat: $scope.mapMarker.getPosition().lat(),
+                lng: $scope.mapMarker.getPosition().lng()
+            };
+
+>>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
             $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${$scope.currentPos.lat},${$scope.currentPos.lng}&key=AIzaSyDg-T5ErBSP7PrxppZiO9OPBA3HW4TXleo`, function (data, status) {
                 if (status === 'success') {
                     $scope.currentAddress = data.results[0].formatted_address;
@@ -361,6 +435,7 @@ function dummyDataInstantiate(scope) {
             Id: 4, Name: 'Van', ImageUrl: '/Assets/Images/van.png', Problems:
             [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
         },
+<<<<<<< HEAD
         {
             Id: 1, Name: 'Bike', ImageUrl: '/Assets/Images/bike.png', Problems:
             [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
@@ -397,13 +472,32 @@ async function makeAjaxRequest()
             {
                 addMarker(data[mechanic].Longitude, data[mechanic].Latitude, 'Mechanic', 'a');
             }
-        },
-        failure: function (err)
+=======
         {
-            console.log(err);
+            Id: 1, Name: 'Bike', ImageUrl: '/Assets/Images/bike.png', Problems:
+            [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
+>>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
+        },
+        {
+            Id: 2, Name: 'Car', ImageUrl: '/Assets/Images/car.png', Problems:
+            [{ Id: 5, Title: 'Problem 5' }, { Id: 6, Title: 'Problem 6' }, { Id: 7, Title: 'Problem 7' }, { Id: 8, Title: 'Problem 8' }]
+        },
+        {
+            Id: 3, Name: 'Truck', ImageUrl: '/Assets/Images/truck.png', Problems:
+            [{ Id: 5, Title: 'Problem 5' }, { Id: 6, Title: 'Problem 6' }, { Id: 7, Title: 'Problem 7' }, { Id: 8, Title: 'Problem 8' }]
+        },
+        {
+            Id: 4, Name: 'Van', ImageUrl: '/Assets/Images/van.png', Problems:
+            [{ Id: 1, Title: 'Problem 1' }, { Id: 2, Title: 'Problem 2' }, { Id: 3, Title: 'Problem 3' }, { Id: 4, Title: 'Problem 4' }]
         }
+<<<<<<< HEAD
     });
 
 =======
+>>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
+=======
+    ];
+
+
 >>>>>>> parent of f019f0c... Merge branch 'master' of https://github.com/malihamza/Serviser
 }
