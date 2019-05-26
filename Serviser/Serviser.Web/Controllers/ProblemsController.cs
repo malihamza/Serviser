ff0cfe7ce13fe4a2c8c8024d60f8e2777706ca76
ehsaan <ehsaan90@outlook.com>
2019-05-26 16:30:07 +0500

    AdminPanel

---
+++ Serviser/Serviser.Web/Controllers/ProblemsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serviser.Web.Controllers
+{
+    public class ProblemsController : Controller
+    {
+        // GET: Problems
+        public ActionResult ViewAllProblems()
+        {
+            return View();
+        }
+
+        public ActionResult AddProblem()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteProblem()
+        {
+            return View();
+        }
+    }
+}
---
+++ Serviser/Serviser.Web/Controllers/VehicleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serviser.Web.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult ViewAllVehicles()
+        {
+            return View();
+        }
+
+        public ActionResult AddVehicle()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteVehicle()
+        {
+            return View();
+        }
+    }
+}
