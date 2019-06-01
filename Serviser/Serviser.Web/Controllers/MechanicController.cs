319be99a45d73fbddf854ef63c9c230ce7affde4
ehsaan <ehsaan90@outlook.com>
2019-06-01 15:02:28 +0500

    MechanicViews

---
+++ Serviser/Serviser.Web/Controllers/MechanicController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serviser.Web.Controllers
+{
+    public class MechanicController : Controller
+    {
+        // GET: Mechanic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult GenerateBill()
+        {
+            return View();
+        }
+
+
+    }
+}
